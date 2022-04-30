af3be6870a17a12fd2af628863f2d9e324ae7035
Roy <roy@codecraftr.nl>
2022-04-30 17:04:55 +0200

    recursion exercise 2 & 3

--- tests/chapter_10_recursion/test_recurse.py
+++ tests/chapter_10_recursion/test_recurse.py
@@ -7,6 +7,24 @@ def print_every_other(low, high):
     print_every_other(low + 2, high)
 
 
+# stack overflows -> hits 0 and misses the base case of 1
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 2)
+
+
+def sum_numbers(low, high):
+    if high > low:
+        return high + sum_numbers(low, high - 1)
+    else:
+        return low
+
+
 class TestRecurse:
     def test_print_every_other(self):
         print_every_other(0, 10)
+
+    def test_sum_should_sum_all_numbers_in_range(self):
+        assert sum_numbers(0, 10) == 55
