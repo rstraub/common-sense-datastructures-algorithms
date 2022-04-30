df0e42d1f57282c8f7dbb334fa2de938e512c3be
Roy <roy@codecraftr.nl>
2022-04-30 17:08:48 +0200

    roadblock progress marker

--- tests/chapter_10_recursion/test_recurse.py
+++ tests/chapter_10_recursion/test_recurse.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def print_every_other(low, high):
     # Base case
     if low > high:
@@ -22,9 +25,37 @@ def sum_numbers(low, high):
         return low
 
 
+def recursive_print(data):
+    print(data)
+
+
 class TestRecurse:
     def test_print_every_other(self):
         print_every_other(0, 10)
 
     def test_sum_should_sum_all_numbers_in_range(self):
         assert sum_numbers(0, 10) == 55
+
+    def test_recursive_print(self):
+        data = [
+            1,
+            2,
+            3,
+            [4, 5, 6],
+            7,
+            [8,
+             [9, 10, 11,
+              [12, 13, 14]
+              ]
+             ],
+            [15, 16, 17, 18, 19,
+                [20, 21, 22,
+                 [23, 24, 25,
+                    [26, 27, 29]
+                  ], 30, 31
+                 ], 32
+             ], 33
+        ]
+
+        recursive_print(data)
+        pytest.fail()
