12d9b39267435b10ad145e9a42ed0ff03527cae7
Roy <roy@codecraftr.nl>
2022-06-06 19:12:10 +0200

    Optimize function using memoization

--- datastructures_algorithms/chapter_12_dynamic_programming/dynamic_programming.py
+++ datastructures_algorithms/chapter_12_dynamic_programming/dynamic_programming.py
@@ -8,3 +8,14 @@ def add_until_100(array):
         return remainder
     else:
         return array[0] + remainder
+
+
+def golomb(n, memo={}):
+    print("golomb")
+
+    if n == 1:
+        return 1
+
+    if n not in memo:
+        memo[n] = 1 + golomb(n - golomb(golomb(n - 1)))
+    return memo[n]
--- tests/chapter_12_dynamic_programming/test_dynamic_programming.py
+++ tests/chapter_12_dynamic_programming/test_dynamic_programming.py
@@ -1,5 +1,6 @@
 from datastructures_algorithms.chapter_12_dynamic_programming.dynamic_programming import (
     add_until_100,
+    golomb,
 )
 
 
@@ -7,3 +8,6 @@ class TestDynamicProgramming:
     def test_add_until_100(self):
         result = add_until_100([20, 30, 60, 45])
         assert result == 95
+
+    def test_golomb(self):
+        assert golomb(7) == 4
